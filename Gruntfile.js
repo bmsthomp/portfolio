c392acdb99bd89336cdf4a8cacd44daa7a639c7b
Brian <bmsthomp@gmail.com>
2020-08-22 14:35:36 -0500

    grunt init. moved to public.

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+    // Load S3 plugin
+    grunt.loadNpmTasks('grunt-aws')
+    // Static Webserver
+    grunt.loadNpmTasks('grunt-contrib-connect')
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        aws: grunt.file.readJSON('aws-keys.json'),
+        s3: {
+            options: {
+                accessKeyId: '<%= aws.accessKeyId %>',
+                secretAccessKey: '<%= aws.secretAccessKey %>',
+                bucket: '<%= aws.bucket %>',
+            },
+            build: {
+                cwd: 'public',
+                src: '**',
+            },
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    base: 'public',
+                    keepalive: true,
+                },
+            },
+        },
+    })
+    // Default task(s).
+    grunt.registerTask('default', ['connect'])
+}
